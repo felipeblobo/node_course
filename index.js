1f9d1753e831d071c2fedd50b22cb46ea0764de9
Felipe Lobo <felipeblobo@gmail.com>
2020-12-17 23:04:59 -0300

    aprimorando as rotas

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 const express = require("express");
 const app = express();
-const handlebars = require('express-handlebars')
-const Post = require('./models/Post')
+const handlebars = require("express-handlebars");
+const Post = require("./models/Post");
 
-
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 app.use(express.json());
-
 
 // configurando a template engine
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
-
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // ROTAS
 
-    app.get('/', (req,res) => {
-        res.render('home')
-    });
+app.get("/", (req, res) => {
+  Post.findAll().then((posts) => {
+    console.log(posts);
+    res.render("home", {posts: posts})
+  })
+})
 
-    app.get('/cad', (req,res) => {
-        res.render('formularios')
-    });
- 
-    app.post('/add', (req,res) => {
-        Post.create({
-            titulo: req.body.titulo,
-            mensagem: req.body.mensagem
-        }).then(() => {    
-            res.redirect('/')
-        }).catch((erro) => {
-            res.send(`Houve um erro: ${erro}`)
-        })
+app.get("/cad", (req, res) => {
+  res.render("formularios");
+});
+
+app.post("/add", (req, res) => {
+  Post.create({
+    titulo: req.body.titulo,
+    mensagem: req.body.mensagem,
+  })
+    .then(() => {
+      res.redirect("/");
     })
+    .catch((erro) => {
+      res.send(`Houve um erro: ${erro}`);
+    });
+});
 
 app.listen(8081, () => {
-    console.log("Servidor rodando na url http://localhost:8081")
-}); 
-
+  console.log("Servidor rodando na url http://localhost:8081");
+});
