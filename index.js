39cdae18dbf6c14244cf20134144d186c8b7b18f
Felipe Lobo <felipeblobo@gmail.com>
2020-12-17 21:58:22 -0300

    Colocando formulários na aplicação

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
+const Post = require('./models/Post')
 
 
 app.use(express.urlencoded())
@@ -15,15 +16,26 @@ app.set('view engine', 'handlebars')
 
 // ROTAS
 
-    app.get('/cad', function(req,res){
+    app.get('/', (req,res) => {
+        res.render('home')
+    });
+
+    app.get('/cad', (req,res) => {
         res.render('formularios')
-    })
+    });
  
-    app.post('/add', function(req,res){
-                res.send('Texto: '+req.body.titulo+' Conteudo: '+req.body.conteudo)
+    app.post('/add', (req,res) => {
+        Post.create({
+            titulo: req.body.titulo,
+            mensagem: req.body.mensagem
+        }).then(() => {    
+            res.redirect('/')
+        }).catch((erro) => {
+            res.send(`Houve um erro: ${erro}`)
+        })
     })
 
-app.listen(8081, function(){
+app.listen(8081, () => {
     console.log("Servidor rodando na url http://localhost:8081")
 }); 
 
